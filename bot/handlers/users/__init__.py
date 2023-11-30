5d044ac576c251520efad696567f809079a55f72
Anton Sh. Postnikov <a.postnikov@uzairports.com>
2023-11-30 16:10:14 +0500

    add auth

--- bot/handlers/users/__init__.py
+++ bot/handlers/users/__init__.py
@@ -2,6 +2,7 @@ from .settings import dp
 from .start import dp
 from .admin_menu import dp
 from .web_app_test import dp
+from .menu import dp
 
 from .helpers import dp
 
---
+++ bot/handlers/users/menu.py
@@ -0,0 +1,20 @@
+import glpi_api
+from aiogram.types import Message
+from loader import dp
+
+
+@dp.message_handler(i18n_text='menu')
+async def _menu_glpi(message: Message):
+
+    URL = 'https://help.uzairports.com//apirest.php'
+    APPTOKEN = '9ZumD7bMJeXrU0aF8V9UVrMobsSwcvqn26e4diPn'
+    USERTOKEN = '6oussx3qkS6ILN77fOmp9ZtC7sWrSXGcCxlmlV3R'
+    try:
+        with glpi_api.connect(url=URL, apptoken=APPTOKEN, auth=('a.postnikov', 'Nokia0910033@')) as glpi:
+            print(glpi.add('Ticket',
+                     {'type': 2, 'urgency': 5, 'name': 'Тестовая заявка', 'content':'Тест примерно'}))
+
+    except glpi_api.GLPIError as err:
+        print(str(err))
+
+    await message.answer('привет')
