049a6966431e416b80b5a62c0cde61af2bfe41e0
Anton Sh. Postnikov <a.postnikov@uzairports.com>
2023-11-30 16:13:59 +0500

    add auth 1

--- bot/handlers/users/menu.py
+++ bot/handlers/users/menu.py
@@ -12,7 +12,7 @@ async def _menu_glpi(message: Message):
     try:
         with glpi_api.connect(url=URL, apptoken=APPTOKEN, auth=('a.postnikov', 'Nokia0910033@')) as glpi:
             print(glpi.add('Ticket',
-                     {'type': 2, 'urgency': 5, 'name': 'Тестовая заявка', 'content':'Тест примерно'}))
+                     {'type': 2, 'urgency': 5, 'name': 'Тестовая заявка', 'content': 'Тест примерно'}))
 
     except glpi_api.GLPIError as err:
         print(str(err))
